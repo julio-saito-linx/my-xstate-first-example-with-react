748fb70bd1a36227f6cafaaf5ccd00c8e734315d
Julio Saito <saitodisse@gmail.com>
2023-07-20 15:01:47 -0300

    name -> customer_name

--- machines/chatMachine.ts
+++ machines/chatMachine.ts
@@ -2,13 +2,13 @@ import { assign, createMachine, interpret } from "xstate";
 
 export const chatMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5QGMAWBDALgYmQV1kwHsBbMAJwH0B3dAO01kuOfQBsBrGgS01UvSUADhVhE6AbQAMAXUSghRWL27j5IAB6IALACYANCACeiABwBGAHTaA7DdMA2G+YCsulwGZd28wF9fhmhY2ORwmOjkmJboyJiqkrLqispxakiaOgbGiLoeVi7aAJxeNnmFhaa6pv6BGFHosFx06GTYsHgARiS80nLpySppoFoI5lKmppZVLg6FNp6mhbpShYYmCB4VlmMuLlI2Ul5S2qY2DjUgQVHI4gBm3OQklM2tRnC9SUqDdOojrja6KYuQrmCbuApLFxrRAeBweSyeWbODwnBy7bwXK6WG50e6PZ4tMDYOhED79L6pH7pP7LQqWca6M4ndEg0zQja2SwOXRoqS6UGHNx+AKXOqWahgNg3VqwVBEaiUEjobh0RVgOh4MkKCnxX6IOHwlwTDy7BxOcoVdn8myWDxSU0eUzaDw2bQuOyYsUSqWkIkQIjPIiYSgYABuYAJZC1IAGlL1CANCONpvN5TZ2Q2DishyR5hRjnR2k9WEsZA1bTAETQlFuRCoQnIRAgeFi0djuupiHMSxtZrNTvGbic7JN8LR2m03NBegOwtqJbLeGwmHI9Fl3CEzAD7UUkTbOqGGVGPa5fYHpiHNitbi5hQcbu8pnGTJsxaii+Xq7o683LFgkrYCh9xSDthi7E8+wcc9L3ZN06XMcwATsbttHGQp9jfSx-yrfha3rRtm1ibAOhiDhgO+eNlg8eF7AsKRzDhN0inTdY7XhRCCndaj3FKDxMJXNdUA3LdKB3OscBI5AyMSckQMPEYqJoiYxgYlEIRYnI+SmRDYUKd1TEdfZqhFLEBO-ITf23ACKGI0jyLjTsEEUyxaJUxj1Ng8YuUdRlcj0TZdEZfwRRJCA4HUK5PjkqkwIQABaBx2TilxLAtNL0vKPiTLFBomkJKKKMcgyHFtO8nQvLNuQnEdHXpN0pDNHk9MKHwi2ykscTxJ4XjAAqHNivMpCkOrNhamZUOqjMUWGziGu5WZgVazDvWlXrZMK2LNnhPICmOLNu0tDNlkmXZXSzHk4X5PNMMXPrQKPBi4SmZYJ1ZN1720K0VlK-YxvKcdbEw7DyGrPDhAIltMDu+ScjzQF6JsJZ0IOAE0StO96RavlzD0UFXCy+cojMn8RLEyJoZio9+S8elEKR-Z9h5KEMzp57dkFbxnBBAnRRLYmLNJ6zyApyi4dpxHdGRxm0YzVD8hmYrnVU502v8IA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QGMAWBDALgYmQV1kwHsBbMAJwH0B3dAO01kuOfQBsBrGgS01UvSUADhVhE6AbQAMAXUSghRWL27j5IAB6IALACYANCACeiABwBGAHTaA7DdMA2G+YCsulwGZd28wF9fhmhY2ORwmOjkmJboyJiqkrLqispxakiaOgbGiLoeVi7aAJxeNnmFhaa6pv6BGFHosFx06GTYsHgARiS80nLpySppoFoI5lKmppZVLg6FNp6mhbpShYYmCB4VlmMuLlI2Ul5S2qY2DjUgQVHI4gBm3OQklM2tRnC9SUqDdOojrja6KYuQrmCbuApLFxrRAeBweSyeWbODwnBy7bwXK6WG50e6PZ4tMDYOhED79L6pH7pP7LQqWca6M4ndEg0zQja2SwOXRoqS6UGHNx+AKXOqWahgNg3VqwVBEaiUEjobh0RVgOh4MkKCnxX6IOHwlwTDy7BxOcoVdn8myWDxSU0eUzaDw2bQuOyYsUSqWkIkQIjPIiYSgYABuYAJZC1IAGlL1CANCONpvN5TZ2Q2DishyR5hRjnR2k9WEsZA1bTAETQlFuRCoQnIRAgeFi0djuupiHMSxtZrNTvGbic7JN8LR2m03NBegOwtqJbLeGwmHI9Fl3CEzAD7UUkTbOqGGVGPa5fYHpiHNitbi5hQcbu8pnGTJsxaii+Xq7o683LFgkrYCh9xSDthi7E8+wcc9L3ZN06XMcwATsbttHGQp9jfSx-yrfha3rRtm1ibAOhiDhgO+eNlg8eF7AsKRzDhN0inTdY7XhRCCndaj3FKDxMJXNdUA3LdKB3OscBI5AyMSckQMPEYqJoiYxgYlEIRYnI+SmRDYUKd1TEdfZqhFLEBO-ITf23ACKGI0jyLjTsEEUyxaJUxj1Ng8YuUdRlcj0TZdEZTDvWlP0wDwSgKEbezQKPWl6VQqRBwnJZQQ0jZQXpZZGV2OYXSNfwRRJCA4HUK5PjkqkwIQABaBx2RqlxLAtO9QR5PS5jOTCGiaQkKooxyDIcW07ydC8s25CcR0dBK9jNdrgR8IsTLFHE8SeF4wH6hzqrzJKEs2Qo3XvY5tBHVCETdKR5tmRbzGW+cohC31ttikZNnhPICmOLNu0tDNlkmXZXSzHk4X5PNMMXV75K7M14UC07WWOqaAZWEb9iO4E7wKWxMOw8hqzw4QCJbTAYaquK80BeibCWdCDgBNErTvekjr5e7+QsTx+K-H8RLEyIKco6n6UQ+n9n2HkoQzcWpnBQVvGcEE+JWkszP5v9rPIYXHP5Lwxbp3QGal5mM1Q-IZiG51VOdB7RRLMANG4DoIyiohdeqrw6WOJLkqKflVnN-YEXvEFxgmJxu0K3wgA */
     id: "chat",
 
     initial: "welcome",
 
     schema: {
-      context: {} as { name: string; messages: string[]; error: any },
+      context: {} as { customer_name: string; messages: string[]; error: any },
       events: {} as
         | {
             type: "back";
@@ -61,7 +61,7 @@ export const chatMachine = createMachine(
     tsTypes: {} as import("./chatMachine.typegen").Typegen0,
 
     context: {
-      name: "",
+      customer_name: "",
       messages: [],
       error: null,
     },
@@ -199,8 +199,7 @@ export const chatMachine = createMachine(
     },
     guards: {
       "can.submit": (context, event) => {
-        console.log("can.submit");
-        return true;
+        return event.value?.trim().length > 0;
       },
     },
   }
