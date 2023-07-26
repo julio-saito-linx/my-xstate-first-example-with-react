c85f0977e0c8a0531eda394b463aa205285b9187
Julio Saito <saitodisse@gmail.com>
2023-07-26 00:39:49 -0300

    initial menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { NavigationMenuDemo } from "@/components/list-item-menu";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex flex-col items-center">
+          <NavigationMenuDemo />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
